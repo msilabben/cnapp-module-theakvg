1e5d39ff7b83c962289f682be1ae72aae0a57055
theakvg <31306222+theakvg@users.noreply.github.com>
2026-05-03 11:02:33 +0000

    enable backend tests

--- backend/test/test_app.py
+++ backend/test/test_app.py
@@ -3,4 +3,4 @@ from starlette.testclient import TestClient
 
 def test_app(client: TestClient):
     res = client.get('/api/health')
-    assert res.status_code == 200
+    assert res.status_code == 100
