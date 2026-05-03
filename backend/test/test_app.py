cd582cbbbd999a14d499a9e1b82249b3a57154d6
theakvg <31306222+theakvg@users.noreply.github.com>
2026-05-03 12:05:02 +0000

    add trivy and fix test

--- backend/test/test_app.py
+++ backend/test/test_app.py
@@ -5,4 +5,4 @@ def test_app(client: TestClient):
     res = client.get('/api/health')
     password = "1oihroighoi234p14ho1ihoihoi4o1aa1e11aa"
     assert res.status_code == 200
-    assert res.password == "1oihroighoi234p14ho1ihoihoi4o1aa1e11aa"
+    assert password == "1oihroighoi234p14ho1ihoihoi4o1aa1e11aa"
